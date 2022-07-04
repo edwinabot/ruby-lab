f2dfd95a5b7402f030812d37423ad39294db5864
Edwin Abot <edwinabot@gmail.com>
2022-07-04 09:43:03 -0300

    Using HTTP the gem, and dumping to temp file

--- phishtank-etl/extract.rb
+++ phishtank-etl/extract.rb
@@ -1,32 +1,38 @@
 require 'byebug'
-require 'net/http'
+require 'http'
 
 class Extractor
     def initialize(domain, path)
-        @domain = domain
-        @path = path
+        @uri = "https://#{domain}/#{path}"
     end
 
     def extract
-        res = self.fetch("https://#{@domain}/#{@path}")
-        res.body
+        res = self.fetch @uri
+        byebug
+        if res == nil
+          puts "Tolis"
+        end
     end
 
     private
     def fetch(uri_str, limit = 10)
-        raise 'Too many HTTP redirects' if limit == 0
+      raise 'Too many HTTP redirects' if limit == 0
 
-        response = Net::HTTP.get_response(URI(uri_str))
-
-        case response
-        when Net::HTTPSuccess then
-          response
-        when Net::HTTPRedirection then
-          location = response['location']
-          warn "redirected to #{location}"
-          fetch(location, limit - 1)
-        else
-          response.value
+      temp = Tempfile.new("phishtank_temp_stream_")
+      res = HTTP.get(uri_str)
+      byebug
+      if res.status.ok?
+        res.body.each do |chunk|
+          byebug
+          temp.write(chunk)
         end
+      elsif res.status.redirect?
+        byebug
+        location = res.headers['Location']
+        fetch(location, limit - 1)
+      else
+        raise "Status Code #{res.code} not handled"
+      end
+      temp
     end
 end
