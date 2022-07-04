228829d9311974453b17ac8613b5c15bb0c7cf1e
Edwin Abot <edwinabot@gmail.com>
2022-07-04 00:00:09 -0300

    pulling the feed

--- phishtank-etl/extract.rb
+++ phishtank-etl/extract.rb
@@ -1,6 +1,32 @@
+require 'byebug'
+require 'net/http'
 
 class Extractor
+    def initialize(domain, path)
+        @domain = domain
+        @path = path
+    end
+
     def extract
-        raise "Not implemented error"
+        res = self.fetch("https://#{@domain}/#{@path}")
+        res.body
+    end
+
+    private
+    def fetch(uri_str, limit = 10)
+        raise 'Too many HTTP redirects' if limit == 0
+
+        response = Net::HTTP.get_response(URI(uri_str))
+
+        case response
+        when Net::HTTPSuccess then
+          response
+        when Net::HTTPRedirection then
+          location = response['location']
+          warn "redirected to #{location}"
+          fetch(location, limit - 1)
+        else
+          response.value
+        end
     end
 end
